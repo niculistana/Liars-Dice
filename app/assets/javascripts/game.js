70dab662bf091f13309500ea4a3e81d809985ebf
Nicu Listana <niculistana@sfs-wifi-dhcp-10-143-161-116.wifi.sfsu.edu>
2016-03-28 18:14:14 -0700

    Prototyped the client

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,33 +1,113 @@
-var game = new Phaser.Game(400, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(740, 600, Phaser.AUTO, 'phaser-window', { preload: preload, create: create });
+var numPlayers = 0;
+var assetsLoaded = false;
+var haveWinner = false;
+var logo;
+var text;
+
+// function Game(){
+//     this.numPlayers = numPlayers;
+// }
 
 function preload() {
-
-    game.load.image('hotdog', 'assets/sprites/hotdog.png');
-
+    // opal stuff
+    // ruby code to scan files
+    // for each file, load sprite
+    // var all_Files = getAllFilesFromFolder("assets/sprites/");
+    // all_Files.forEach(logArrayElements);
+    game.load.image('dollars', 'assets/sprites/dollar_sign.png');
+    game.load.image('logo', 'assets/sprites/liars_dice_logo.png');
 }
 
 function create() {
+    game.stage.backgroundColor = "#fff";
 
-    //  You can set the background color of your game in the following ways:
+    logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
 
-    game.stage.backgroundColor = "#4488AA";
-    // game.stage.backgroundColor = 0x4488aa;
-    // game.stage.backgroundColor = 'rgb(68, 136, 170)';
-    // game.stage.backgroundColor = 'rgba(68, 136, 170, 0.5)';
+    logo.anchor.setTo(0.5, 0.5);
+    logo.alpha = 0;
 
-    game.add.image(game.world.centerX, game.world.centerY, 'hotdog').anchor.set(0.5);
+    game.add.tween(logo).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true);
+    state = game.add.text(game.world.centerX, game.world.centerY, "",{ font: "12px Arial", fill: "#ff0044", align: "center" });
 
-    //  Click for a new background color
-    game.input.onDown.add(changeColor, this);
-
+    waitGame();
 }
 
-function changeColor() {
+function waitGame(){
+    numPlayers++;
+    logo.alpha = 1;
+    state.text = "";
+    console.log(numPlayers);
+    if(!isRoomFull()) {
+        setTimeout("waitGame()", 3000);
+    } else {
+        numPlayers = 0;
+        startGame();
+    }
+}
 
-    var c = Phaser.Color.getRandomColor(50, 255, 255);
+function startGame() {
+    if(!isLoaded()) {
+        logo.alpha = 0;
+        state.text = "Start;";
+        // alert("Game has started!");
+        setTimeout("startGame()", 5000);
+        assetsLoaded = true;
+    } else {
+        assetsLoaded = false;
+        continueGame();
+    }
+}
 
-    game.stage.backgroundColor = c;
+function continueGame() {
+    if(!isContinue()) {
+        state.text = "Continue;";
+        // alert("Game is going on...");
+        setTimeout("continueGame()", 2000);
+        haveWinner = true;
+    } else {
+        // alert("Game has ended...");
+        state.text = "End;";
+        haveWinner = false;
 
-    game.fd.record(4);
+        emitter = game.add.emitter(game.world.centerX, 250, 200);
+        emitter.makeParticles('dollars');
 
+        emitter.setRotation(0, 0);
+        emitter.setAlpha(0.3, 0.8);
+        emitter.setScale(0.5, 1);
+        emitter.gravity = 0;
+
+         // false means don't explode all the sprites at once, but instead release at a rate of 20 particles per frame
+         // The 5000 value is the lifespan of each particle
+        emitter.start(false, 4000, 20);
+        setTimeout(function(){
+            emitter.destroy();
+            waitGame();
+        }, 3000);
+    }
+}
+
+function isRoomFull() {
+    if (numPlayers === 4) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isLoaded() {
+    if (assetsLoaded) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isContinue() {
+    if (haveWinner) {
+        return true;
+    } else {
+        return false;
+    }
 }
