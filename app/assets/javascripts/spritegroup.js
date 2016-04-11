1c6cb89e3ba62fd04b550734924caf4311913e19
Nicu Listana <niculistana@Nicus-MacBook-Pro.local>
2016-04-11 01:11:34 -0700

    Added spritegroup for rendering sprites in groups

--- app/assets/javascripts/spritegroup.js
+++ app/assets/javascripts/spritegroup.js
@@ -1,19 +1,31 @@
-function SpriteGroup (spriteName, spriteGroup, numSprite, spriteOriginPosX, spriteOriginPosY) {
+function SpriteGroup (spriteName, spriteGroup, objectPool, numSprite, spriteOriginPosX, spriteOriginPosY) {
   this.spriteName = spriteName;
   this.spriteGroup = spriteGroup;
   this.numSprite = numSprite;
   this.spriteOriginPosX = spriteOriginPosX;
   this.spriteOriginPosY = spriteOriginPosY;
 
-  this.renderSprites = function () {
+  this.renderSprites = function (shape) {
     var spriteFileName = "";
     var spritePosX = spriteOriginPosX;
     var spritePosY = spriteOriginPosY;
 
-    for (var i=1; i <= numSprite; i++) {
-      spriteFileName = spriteName + i;
-      spriteGroup.add(game.add.sprite(spritePosX, spritePosY, spriteFileName));
-      spritePosX += 100;
+    // todo: renders in an octagonal fashion
+
+    // renders in a box like fashion
+    if (shape == "box") {
+      console.log(objectPool.allObjects.length);
+      for (var index in objectPool.allObjects) {
+        if (objectPool.allObjects[index].id > 0) {
+          spriteFileName = spriteName + objectPool.allObjects[index].id;
+          spriteGroup.add(game.add.sprite(spritePosX, spritePosY, spriteFileName));
+          spritePosX += 100;
+          if (index % 5 === 4) {
+            spritePosY+= 100;
+            spritePosX = spriteOriginPosX;
+          }
+        }
+      }
     }
   };
 }
