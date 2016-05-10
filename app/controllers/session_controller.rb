fcf65c2394f9f4840afbe39d9fc29d3aad90ce57
Nicu Listana <niculistana@Nicus-MacBook-Pro.local>
2016-05-10 01:22:16 -0700

    Fixed lobby issue by implementing recent_user protocol

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -13,10 +13,18 @@ class SessionController < ApplicationController
     end
   end
 
-  def user_username
+  # def user_username
+  #   respond_to do |format|
+  #     uname_message = {:status => "ok", :uname => current_user.username}
+  #     format.json {render :json => uname_message}
+  #   end
+  # end
+
+  def recent_user
+    @game_user = GameUser.where({game_id: session[:game_id]}).order(:updated_at).last
     respond_to do |format|
-      uname_message = {:status => "ok", :uname => current_user.username}
-      format.json {render :json => uname_message}
+      id_message = {:status => "ok", :uid => @game_user.user_id, :uname => @game_user.user.username, :dice => @game_user.dice}
+      format.json {render :json => id_message}
     end
   end
 end
